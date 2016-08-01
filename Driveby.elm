473e13684612dfd081aabcb9d1e026cafa9ec612
alltonp <paulallton@gmail.com>
2016-08-01 09:13:08 +0100

    move update into Driveby

--- Driveby.elm
+++ Driveby.elm
@@ -8,13 +8,14 @@ import Html exposing (..)
 import Task
 
 
-driveby tests update subscriptions checker =
+driveby tests subscriptions checker =
   App.program
     { init = (Model tests, asFx Start )
     , view = view
-    , update = update
+    , update = update checker
     , subscriptions = subscriptions
     }
+
 
 type alias Step =
   { id: String
@@ -49,6 +50,44 @@ type Msg
 type alias Model =
   { commands : List Step
   }
+
+
+--TODO: this will be the driveby update ...
+--TODO: we will probably need our own to handle DriveBy.Msg ... like the DatePicker ...
+--update : Msg -> Model -> (Model, Cmd Msg)
+update checker msg model =
+  case msg of
+    Start ->
+      let
+        next = List.filter (\s -> not s.executed) model.commands |> List.head
+        cmd = case next of
+            Just c ->
+              let d = Debug.log "Driveby" (c.request.command ++ " " ++ (toString c.request.args) )
+              in checker c --check
+            Nothing -> asFx (Exit ("Passed") )
+      in
+      ( model, cmd )
+
+    Suggest response ->
+      let
+        current = List.filter (\s -> s.id == response.id) model.commands
+        steps' = List.map (\s -> if s.id == response.id then Step s.id s.request True else s ) model.commands
+        model' = { model | commands = steps' }
+        --TODO: go with Script, Step, Command, Result etc
+        --TODO: send ExampleFailure if response has failures
+        --TODO: Start should be NextStep
+        next = if List.isEmpty response.failures then asFx Start
+               else asFx (Exit ("Failed: " ++ (toString response.failures) ++ " running " ++ (toString current)) )
+      in
+      ( model', next )
+
+    --TODO: is this Failed really?
+    Exit message ->
+      let
+        --TODO: this is odd, lets do in js instead ...
+        d = Debug.log "Driveby" message
+      in
+      ( model, checker (Step "999" (Request "close" [] ) False) {-check-} )
 
 
 view : Model -> Html Msg
--- Main.elm
+++ Main.elm
@@ -6,7 +6,7 @@ import Driveby exposing (..)
 
 
 main =
-   driveby commands update subscriptions checker
+   driveby commands subscriptions (checker check)
 
 
 --TODO: specify this using functions, to ensure the correct args ... click id etc
@@ -31,44 +31,7 @@ port check : Step -> Cmd msg
 
 --check : Msg -> (Step -> Cmd msg)
 checker m p =
-    p m
-
---TODO: this will be the drivby update ...
---TODO: we will probably need our own to handle DriveBy.Msg ... like the DatePicker ...
-update : Msg -> Model -> (Model, Cmd Msg)
-update msg model =
-  case msg of
-    Start ->
-      let
-        next = List.filter (\s -> not s.executed) model.commands |> List.head
-        cmd = case next of
-            Just c ->
-              let d = Debug.log "Driveby" (c.request.command ++ " " ++ (toString c.request.args) )
-              in checker c check
-            Nothing -> asFx (Exit ("Passed") )
-      in
-      ( model, cmd )
-
-    Suggest response ->
-      let
-        current = List.filter (\s -> s.id == response.id) model.commands
-        steps' = List.map (\s -> if s.id == response.id then Step s.id s.request True else s ) model.commands
-        model' = { model | commands = steps' }
-        --TODO: go with Script, Step, Command, Result etc
-        --TODO: send ExampleFailure if response has failures
-        --TODO: Start should be NextStep
-        next = if List.isEmpty response.failures then asFx Start
-               else asFx (Exit ("Failed: " ++ (toString response.failures) ++ " running " ++ (toString current)) )
-      in
-      ( model', next )
-
-    --TODO: is this Failed really?
-    Exit message ->
-      let
-        --TODO: this is odd, lets do in js instead ...
-        d = Debug.log "Driveby" message
-      in
-      ( model, checker (Step "999" (Request "close" [] ) False) check )
+    m p
 
 
 --TODO: need to be exposed somehow
