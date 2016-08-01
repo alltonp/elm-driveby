1937f52f1763d487e813e4dd1eb86229b741270e
alltonp <paulallton@gmail.com>
2016-08-01 10:33:56 +0100

    add an autotest ...

---
+++ autotest.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# requires: brew install entr
+
+#cd elm
+
+#multimap/*.elm typeahead/*.elm taggage/*.elm
+#typeahead/*.elm multimap/*.elm taggage/*.elm
+
+#elm-make src/Main.elm --output chrome/elm.js
+ls `find . -name '*.elm' -not -path '*elm-stuff*' -print` | entr sh -c 'clear; rm tests.js; elm-make `find . -name \*.elm -not -path \*elm-stuff\*  -print` --output tests.js; if [ -f tests.js ]; then eval "./phantomjs runner.js"; fi;'
+
+#ls `find . -name '*.elm'  -print` | entr sh -c 'clear; rm ../chrome/elm.js; elm-make `find . -name \*.elm -print` --output ../chrome/elm.js'
+#ls `find . -name '*.elm'  -print` | entr sh -c 'elm-make `find . -name \*.elm -print` --output ../chrome/elm.js'
