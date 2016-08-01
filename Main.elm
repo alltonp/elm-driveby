85f1655d1e311177c12c64ac67dd9f5a5ac83631
alltonp <paulallton@gmail.com>
2016-08-01 06:52:57 +0100

    be more explicit

--- Main.elm
+++ Main.elm
@@ -12,7 +12,7 @@ import Driveby exposing (..)
 main =
   App.program
     { init = init
-    , view = view
+    , view = Driveby.view
     , update = update
     , subscriptions = subscriptions
     }
