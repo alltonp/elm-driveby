77a8b2437a6cadfd0a089bf27cc058deb75f7c57
alltonp <paulallton@gmail.com>
2016-08-17 07:34:40 +0100

    a little less documentation

--- src/Driveby/Model.elm
+++ src/Driveby/Model.elm
@@ -1,6 +1,6 @@
 module Driveby.Model exposing (..)
 
-{-| A Step holding a Command to execute -}
+
 type alias Step =
   { id : Int
   , command : Command
@@ -11,21 +11,18 @@ type alias Step =
 --TODO: consider id/selector being a a first class thing, at least a Maybe ...
 --TODO: consider value being a a first class thing, at least a Maybe ...
 --TODO: consider expected being a a first class thing, at least a Maybe ...
-{-| A Command to execute -}
 type alias Command =
   { name : String
   , args : List String
   }
 
 
-{-| A Condition to check -}
 type alias Condition =
   { description : String
   , args : List String
   }
 
 
-{-| The Context of an executing Script -}
 type alias Context =
   { localPort : Int
   , browserId : Int
